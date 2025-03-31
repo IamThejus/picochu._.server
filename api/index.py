04c4e7055d2b1f2c8635ce4834a8e9693544aaa2
IamThejus <thejusasokan123@gmail.com>
2025-03-31 11:48:14 +0530

    Created a engine for index

--- api/index.py
+++ api/index.py
@@ -1,53 +1,6 @@
 from flask import Flask,request,jsonify
-from pymongo import MongoClient
-import requests
-from bs4 import BeautifulSoup
-import requests
+from engine import *
 app=Flask(__name__)
-
-
-######################################## functions ########################################################################################
-
-
-MONGO_URI = "mongodb+srv://picochu:picochu@2hekav0.mongodb.net/?retryWrites=true&w=majority"
-
-def get_attendance_list(data):
-    client = MongoClient(MONGO_URI)
-
-    # Select database & collection
-    db = client["attendance_system"]
-    collection = db["attendance_records"]
-
-    data=[]
-    for record in collection.find(data):
-        del record["_id"]
-        return record
-    
-
-
-url="https://sngce.etlab.in/user/login"
-
-
-
-            
-def get_loggedin(usrid,passwd):
-    payload={
-        'LoginForm[username]':usrid,
-        'LoginForm[password]':passwd,
-        "yt0":""
-    }
-
-    session=requests.Session()
-
-    request=session.post(url,data=payload)
-    data=BeautifulSoup(request.text,"html.parser")
-    result={}
-    message=data.find(class_="flash-error")
-    if message is None:
-        result={"request":True,"session":session}
-    else:
-        result={"request":False,"message":message.text.strip()}
-    return result
 
 
 
